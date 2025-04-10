0b11ead841e7dd8960970476ec4058c411d16eab
Man <tavan1259@gmail.com>
2025-04-10 10:10:19 +0700

    remove data

--- man.js
+++ man.js
@@ -2,4 +2,3 @@ console.log("Hello, world!");
 console.log("This is a test.");
 console.log("This is a test."); // This is a test
 console.log("This is a test."); // This is a test
-console.log("This is a test."); // This is a test
