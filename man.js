0ec3166a4960c2336a206585c22f534ad6fc4dd3
Man <tavan1259@gmail.com>
2025-04-10 09:39:33 +0700

    change js

---
+++ man.js
@@ -0,0 +1,5 @@
+console.log("Hello, world!");
+console.log("This is a test.");
+console.log("This is a test."); // This is a test
+console.log("This is a test."); // This is a test
+console.log("This is a test."); // This is a test
